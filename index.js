ecd12bd7be7ee8f8d88f2697e0ca14d79ccfd235
Stevenbm95 <stevenbm95@gmail.com>
2025-05-06 13:15:12 -0500

    cambios para poder generar el cliente de prisma

--- index.js
+++ index.js
@@ -30,6 +30,5 @@ app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-export default (req, res) => {
-  app(req, res); // Esta es la forma correcta de invocar Express en Vercel.
-};
+export default app;
+
